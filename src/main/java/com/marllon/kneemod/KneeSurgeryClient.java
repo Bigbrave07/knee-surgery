da497235950b0ed4c7fc108c22777b414915e43c
Marlon <marlonlaufs@gmail.com>
2024-11-21 21:32:23 +0100

    aa

---
+++ src/main/java/com/marllon/kneemod/KneeSurgeryClient.java
@@ -0,0 +1,10 @@
+package com.marllon.kneemod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class KneeSurgeryClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/marllon/kneemod/KneeSurgeryDataGenerator.java
@@ -0,0 +1,11 @@
+package com.marllon.kneemod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class KneeSurgeryDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
