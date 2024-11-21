a9ff6f7d62c4acdad5e27a283956bdc92aa5b7b9
Marlon <marlonlaufs@gmail.com>
2024-11-21 23:07:48 +0100

    aa

--- src/main/java/com/marllon/kneemod/item/ModItems.java
+++ src/main/java/com/marllon/kneemod/item/ModItems.java
@@ -6,16 +6,36 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
 
+import java.util.function.Function;
 
 
 public class ModItems {
 
-    public static final Item PINK_GARNET = registerItem(name: "pink_garnet", new Item(Item.Settings()));
+    public static final Item PINK_GARNET = registerItem("pink_garnet", Item::new);
+    private static Item registerItem(String name, Function<Item.Settings, Item> function) {
+        return Registry.register(Registries.ITEM, Identifier.of(KneeSurgery.MOD_ID, name), function.apply(new Item.Settings().registryKey(keyOfItem(name))));
+    }
+
+    private static RegistryKey<Item> keyOfItem(String name) {
+        return RegistryKey.of(RegistryKeys.ITEM, Identifier.of(KneeSurgery.MOD_ID, name));
+    }
+
+    public static void registerModItems() {
+        KneeSurgery.LOGGER.info("Registering Mod Items for " + KneeSurgery.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(fabricItemGroupEntries -> {
+                    fabricItemGroupEntries.add(PINK_GARNET);
+        });
+    }
+
+    /*  public static final Item PINK_GARNET = registerItem("pink_garnet", new Item(new Item.Settings()));
 
 
-    private static Item registerItem(String name, Item item) {
+   private static Item registerItem(String name, Item item) {
 
 
         return Registry.register(Registries.ITEM, Identifier.of(KneeSurgery.MOD_ID, name), item);
@@ -29,5 +49,5 @@ public class ModItems {
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(fabricItemGroupEntries -> {
             fabricItemGroupEntries.add(PINK_GARNET);
         });
-    }
+    } */
 }
