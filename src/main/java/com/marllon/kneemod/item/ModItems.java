6e7a8ed686626465b5f20f1b5eec85bc51143a8b
Marlon <marlonlaufs@gmail.com>
2024-11-21 23:43:54 +0100

    aa

--- src/main/java/com/marllon/kneemod/item/ModItems.java
+++ src/main/java/com/marllon/kneemod/item/ModItems.java
@@ -16,6 +16,7 @@ import java.util.function.Function;
 public class ModItems {
 
     public static final Item PINK_GARNET = registerItem("pink_garnet", Item::new);
+    public static final Item RAW_PINK_GARNET = registerItem("raw_pink_garnet", Item::new);
     private static Item registerItem(String name, Function<Item.Settings, Item> function) {
         return Registry.register(Registries.ITEM, Identifier.of(KneeSurgery.MOD_ID, name), function.apply(new Item.Settings().registryKey(keyOfItem(name))));
     }
@@ -29,6 +30,7 @@ public class ModItems {
 
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(fabricItemGroupEntries -> {
                     fabricItemGroupEntries.add(PINK_GARNET);
+                    fabricItemGroupEntries.add(RAW_PINK_GARNET);
         });
     }
 
